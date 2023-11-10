b0b6be60c6eb7224b0ef2709d17b1a48be55f812
utkarsh-90 <150446119+utkarsh-90@users.noreply.github.com>
2023-11-10 18:33:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body')
+const buttons = document.querySelectorAll('.btn')
+
+buttons.forEach((button) => {
+    // console.log(button);
+    button.addEventListener('click', (e) => {
+        console.log(e.target.id);
+        if (e.target.id === 'yellow') {
+        body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+        body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'grey') {
+        body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'red') {
+        body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+        body.style.backgroundColor = e.target.id;
+        }
+    })
+})
